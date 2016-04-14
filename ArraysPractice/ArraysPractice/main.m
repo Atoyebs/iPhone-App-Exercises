b0969b8a05365d899100fb6bf78168e62aada17f
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-14 15:58:24 +0100

    Adding ArraysPractice Project to GitHub
    This project is for practicing the manipulation of arrays of all kinds
    including sorting and filtering and ordering etc

---
+++ ArraysPractice/ArraysPractice/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArraysPractice
+//
+//  Created by Inioluwa Work Account on 14/04/2016.
+//  Copyright © 2016 Inioluwa Work Account. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
